5e457d8a48f1b730391d01b03396656e7c1070a2
Šimon Frőmel <120651160+Bobriky@users.noreply.github.com>
2023-10-17 12:29:08 +0200

    Add files via upload

---
+++ test 3.10.2023/DatoveTypy/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DatoveTypy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVysl = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblVysl
+            // 
+            this.lblVysl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblVysl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblVysl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblVysl.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblVysl.Location = new System.Drawing.Point(39, 78);
+            this.lblVysl.Name = "lblVysl";
+            this.lblVysl.Size = new System.Drawing.Size(517, 19);
+            this.lblVysl.TabIndex = 5;
+            this.lblVysl.Text = " - hodnota -";
+            this.lblVysl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Výsledek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 161);
+            this.Controls.Add(this.lblVysl);
+            this.Controls.Add(this.label6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(380, 200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datové typy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblVysl;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
