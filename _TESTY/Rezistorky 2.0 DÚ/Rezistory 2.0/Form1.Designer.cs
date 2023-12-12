aee4f2fcf1d591c9fc1c61ca803efa62ea17a9c6
Šimon Frőmel <fromelsimon2@gmail.com>
2023-12-12 22:23:12 +0100

    dú

---
+++ _TESTY/Rezistorky 2.0 DÚ/Rezistory 2.0/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace Rezistory_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxValue2 = new System.Windows.Forms.TextBox();
+            this.txtBoxValue1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBoxParalel = new System.Windows.Forms.TextBox();
+            this.txtBoxSerial = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBoxValue2);
+            this.groupBox1.Controls.Add(this.txtBoxValue1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Zadání";
+            this.groupBox1.TextChanged += new System.EventHandler(this.groupBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(188, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ω";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ω";
+            // 
+            // txtBoxValue2
+            // 
+            this.txtBoxValue2.Location = new System.Drawing.Point(48, 59);
+            this.txtBoxValue2.Name = "txtBoxValue2";
+            this.txtBoxValue2.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxValue2.TabIndex = 1;
+            this.txtBoxValue2.Text = "1";
+            this.txtBoxValue2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBoxValue2.TextChanged += new System.EventHandler(this.groupBox1_TextChanged);
+            // 
+            // txtBoxValue1
+            // 
+            this.txtBoxValue1.Location = new System.Drawing.Point(48, 25);
+            this.txtBoxValue1.Name = "txtBoxValue1";
+            this.txtBoxValue1.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxValue1.TabIndex = 0;
+            this.txtBoxValue1.Text = "1";
+            this.txtBoxValue1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBoxValue1.TextChanged += new System.EventHandler(this.groupBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "R2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R1";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(87, 140);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "=";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtBoxParalel);
+            this.groupBox2.Controls.Add(this.txtBoxSerial);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(12, 182);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(225, 99);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Vypočteno";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(188, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Ω";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(188, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Ω";
+            // 
+            // txtBoxParalel
+            // 
+            this.txtBoxParalel.Location = new System.Drawing.Point(48, 63);
+            this.txtBoxParalel.Name = "txtBoxParalel";
+            this.txtBoxParalel.ReadOnly = true;
+            this.txtBoxParalel.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxParalel.TabIndex = 1;
+            this.txtBoxParalel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBoxSerial
+            // 
+            this.txtBoxSerial.Location = new System.Drawing.Point(48, 32);
+            this.txtBoxSerial.Name = "txtBoxSerial";
+            this.txtBoxSerial.ReadOnly = true;
+            this.txtBoxSerial.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxSerial.TabIndex = 0;
+            this.txtBoxSerial.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 17);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Rp";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 17);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Rs";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Enabled = false;
+            this.btnReset.Location = new System.Drawing.Point(81, 292);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(162, 292);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "Konec";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(249, 327);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Rezistory 2.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxValue2;
+        private System.Windows.Forms.TextBox txtBoxValue1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBoxParalel;
+        private System.Windows.Forms.TextBox txtBoxSerial;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
+
---
+++ _TESTY/Rezistorky 2.0 DÚ/Rezistory 2.0/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rezistory_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float number1 = float.Parse(txtBoxValue1.Text);
+                float number2 = float.Parse(txtBoxValue2.Text);
+                if (number2 != 0 && number1 != 0)
+                {
+                    txtBoxSerial.Text = (number1 + number2 + ".00").ToString();  //   O_o
+                    txtBoxParalel.Text = (number1 * number2 / (number1 + number2)).ToString();
+                }
+                else
+                    MessageBox.Show("Takže my budeme dělit nulou?");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Zadané hodnoty buď nejsou, nebo jsou špatně zadány!");
+            }
+            btnReset.Enabled = true;
+        }
+
+        private void groupBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtBoxValue1.Text == "" && txtBoxValue2.Text == "")
+                btnReset.Enabled = false;
+            else
+                btnReset.Enabled = true;
+            txtBoxSerial.Text = txtBoxParalel.Text = "";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtBoxValue1.Text = txtBoxValue2.Text = "1";
+            txtBoxSerial.Text = txtBoxParalel.Text = "";
+            btnReset.Enabled = false;
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Opravdu chcete skončit?", "Ukončení aplikace", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result == DialogResult.OK)
+                Close();
+        }
+    }
+}
