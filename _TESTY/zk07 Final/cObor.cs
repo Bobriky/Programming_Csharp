5535653c0dbbc6f83b6330f6c3fce706d77f75e3
Šimon Frőmel <fromelsimon2@gmail.com>
2024-06-18 14:55:13 +0200

    huh

---
+++ _TESTY/zk07 Final/cObor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tridaZaci
+{
+    class cObor
+    {
+        public string Zkratka { get; set; }
+
+        public string Nazev { get; set; }
+
+        public string Cislo { get; set; }
+
+        public string SVP { get; set; }
+
+        public int Delka { get; set; }
+
+        public string Zkouska { get; set; }
+
+        public cObor(string zkratka, string nazev, string cislo, int delka, string zkouska)
+        {
+            Zkratka = zkratka;
+            Nazev = nazev;
+            Cislo = cislo;
+            Delka = delka;
+            Zkouska = zkouska;
+        }
+        public cObor(string zkratka, string nazev, string cislo, int delka)
+        {
+            Zkratka = zkratka;
+            Nazev = nazev;
+            Cislo = cislo;
+            Delka = delka;
+        }
+        public cObor(string zkratka, string nazev, string cislo)
+        {
+            Zkratka = zkratka;
+            Nazev = nazev;
+            Cislo = cislo;
+        }
+        public cObor(string zkratka, string nazev)
+        {
+            Zkratka = zkratka;
+            Nazev = nazev;
+        }
+        public cObor(string zkratka)
+        {
+            Zkratka = zkratka;
+        }
+    }
+}
